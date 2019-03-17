41fe49388f96b277848b31e9a4225c9613f68c45
belindanovoa <novoa.belinda@gmail.com>
2019-03-17 16:52:03 -0700

    upload all files just incase

---
+++ app/data/friends.js
@@ -0,0 +1,76 @@
+json {
+    "name": "Dwight Schrute",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Dwight_Schrute.jpg/220px-Dwight_Schrute.jpg",
+    "scores": [
+        5, 5, 1, 2, 1, 2, 5, 5, 2, 3
+    ]
+}
+
+{
+    "name": "Jim Halpert",
+    "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2017-08/9/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-22381-1502291405-3.jpg?downsize=700:*&output-format=auto&output-quality=auto",
+    "scores": [5, 3, 5, 3, 1, 2, 2, ,3 5,
+    ]
+} {
+    "name": "Pam Beesly",
+    "photo": "https://www.google.com/search?tbm=isch&q=pam+beesly&chips=q:pam+beesly,g_1:happy:MrmTMijiChE%3D,g_1:jenna+fischer&usg=AI4_-kQYlYXEsibCWnqETgmgTzAKu0QZLQ&sa=X&ved=0ahUKEwi14ZX2rorhAhUFqp4KHei2DtwQ4lYIKigB&biw=1920&bih=968&dpr=1#imgrc=LQbHSig4sYpy_M:",
+    "scores": [5, 3, 2, 5, 3, 3, 2, 4, 1,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+    ]
+}
+
+{
+    "name": "",
+    "photo": "",
+    "scores": [, , , , , , , , ,
+
+    ]
+}
